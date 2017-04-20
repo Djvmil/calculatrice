371a7d466a87abc85e8e1f8bcc179e6800e292c1
Moustapha S. Dieme <Moustapha S. Dieme>
2017-04-20 12:13:16 +0000

    1er commit

---
+++ FonctionCalculatrice.h
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int rech_1erPosCar(char nombre[], char car)//"1"Fonction qui permet de rechercher un caratere et retourner sa posision, qui est utiliser dans la fonction ControleOperation
+{
+    int i,j;
+    for(i=0; nombre[i]; i++)
+    {
+        if(nombre[i]==car)
+            return i;
+    }
+    return -1;
+}
+
+int ControleOperation(char chaine[])
+{
+int i,j;
+    if(chaine[0]!='/' && chaine[0]!='*')
+        for(i=0;chaine[i];i++)
+        {
+            if(rech_1erPosCar("0123456789/*-+%",chaine[i])==-1)
+                return 0;
+        }
+    else
+        return 0;
+return 1;
+}
+
+int OccurrenceMD(char chaine[])
+{
+int i,j;
+    for(i=0;chaine[i];i++)
+    {
+        if(chaine[i]=='/' && chaine[i+1]=='*' || chaine[i]=='*' && chaine[i+1]=='/')
+                return 0;
+    }
+return 1;
+}
+
---
+++ calculatrice.c
@@ -0,0 +1,16 @@
+#include"FonctionCalculatrice.h"
+
+int main()
+{
+char operation[101];
+int i,j,operation1[100];
+
+    do
+    {
+        printf("saisir votre operation");
+        gets(operation);
+    } while(ControleOperation(operation)==0 || OccurrenceMD(operation)==0);
+
+    for()
+
+}
